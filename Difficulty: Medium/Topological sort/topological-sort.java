f16b6a62c84edcacd0c421e584991c061723f98d
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-14 08:26:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,36 +1,47 @@
 class Solution {
     public static ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-         ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
-         Stack<Integer> st=new Stack<>();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         boolean vis[]=new boolean[V];
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int []pair:edges){
-           int u=pair[0];
-           int v=pair[1];
-           adj.get(u).add(v);
+        for(int []edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
         }
+        int []indegree=new int[V];
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                DFS(adj,i,vis,st);
+            for(int v:adj.get(i)){
+                indegree[v]++;
+            }
+        }
+         Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
             }
         }
         ArrayList<Integer> res=new ArrayList<>();
-        while(!st.isEmpty()){
-            res.add(st.pop());
-        }
+        BFS(adj,indegree,q,res);
         return res;
+        
     }
-public static void DFS(ArrayList<ArrayList<Integer>> adj,int u,boolean []vis,Stack<Integer> st)
-        {
-             vis[u]=true;
-             for(int v:adj.get(u)){
-                 if(!vis[v]){
-                    DFS(adj,v,vis,st);
-                 }
-             }
-             st.push(u);
-        }
+    public static ArrayList<Integer> BFS(ArrayList<ArrayList<Integer>> adj,int []indegree,Queue<Integer> q, ArrayList<Integer> res){
+       while(!q.isEmpty()){
+           int node=q.poll();
+           res.add(node);
+           for(int v:adj.get(node)){
+               indegree[v]--;
+               if(indegree[v]==0){
+                   q.offer(v);
+               }
+           }
+       } 
+       if(res.size()!=adj.size()){
+           return new ArrayList<>();
+       }
+       return res;
     }
+}
