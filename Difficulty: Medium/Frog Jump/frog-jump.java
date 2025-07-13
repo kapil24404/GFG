ce4e64bdf63e296ca2fab247db3d8e8b6d254cb4
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-13 23:52:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -4,20 +4,16 @@ class Solution {
         // code here
         int n=height.length;
         int dp[]=new int[n+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i]=-1;
-        }
+        Arrays.fill(dp,-1);
         return helper(n-1,height,dp);
     }
-    public int helper(int i,int height[],int dp[]){
+    public static  int helper(int i,int height[],int dp[]){
         if(i==0) return 0;
         if(dp[i]!=-1) return dp[i];
-        int firstjump=helper(i-1,height,dp)+Math.abs(height[i]-height[i-1]);
-        int secondjump = Integer.MAX_VALUE;
-        if(i>1){
-             secondjump=helper(i-2,height,dp)+Math.abs(height[i]-height[i-2]);
-        }
-        dp[i]=Math.min(firstjump,secondjump);
+        int onestep=helper(i-1,height,dp)+Math.abs(height[i]-height[i-1]);
+        int twostep = Integer.MAX_VALUE;
+        if(i>1) twostep=helper(i-2,height,dp)+Math.abs(height[i]-height[i-2]);
+        dp[i]=Math.min(onestep,twostep);
         return dp[i];
     }
 }
