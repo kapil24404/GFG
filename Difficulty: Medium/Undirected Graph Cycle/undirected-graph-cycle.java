e29df1d936e948d3d86bf56d647f491244bdbf57
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-15 11:00:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,48 +1,33 @@
 class Solution {
-    class Pair {
-    int first, second;
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
     public boolean isCycle(int V, int[][] edges) {
         // Code here
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int edge[]: edges){
-            int u=edge[0];
-            int v=edge[1];
+        for(int pair[]:edges){
+            int u=pair[0];
+            int v=pair[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        boolean[] vis=new boolean[V];
+        boolean vis[]=new boolean[V];
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(BFS(adj,i,vis,-1)) return true;
+                if(DFS(adj,i,vis,-1)) return true;
             }
         }
         return false;
     }
-   public boolean BFS(ArrayList<ArrayList<Integer>> adj,int u,boolean[] vis,int parent){
-       Queue<Pair> q=new LinkedList<>();
-       q.add(new Pair(u,parent));
-       vis[u]=true;
-       while(!q.isEmpty()){
-           Pair a=q.poll();
-           int src=a.first,dst=a.second;
-           for(int v:adj.get(src)){
-               if(!vis[v]){
-                   vis[v]=true;
-                   q.add(new Pair(v,src));
-               }
-               else if(v!=dst){
-                   return true;
-               }
-           }
-       }
-       return false;
-}
+    public static boolean DFS(ArrayList<ArrayList<Integer>> adj,int u,boolean vis[],int parent){
+        if(vis[u]) return false;
+        vis[u]=true;
+        for(int v:adj.get(u)){
+            if(!vis[v]){
+               if(DFS(adj,v,vis,u)) return true;
+            }
+            else if(v!=parent) return true;
+        }
+        return false;
+    }
 }
