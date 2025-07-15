0f3078a0e3ff1ba2f962a1ea20802cb76e9196e7
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-15 13:43:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,36 +1,43 @@
 class Solution {
     public static ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-         ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
-         Stack<Integer> st=new Stack<>();
-        boolean vis[]=new boolean[V];
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int []pair:edges){
-           int u=pair[0];
-           int v=pair[1];
-           adj.get(u).add(v);
+        for(int pair[]:edges){
+            int u=pair[0];
+            int v=pair[1];
+            adj.get(u).add(v);
         }
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                DFS(adj,i,vis,st);
+        int indegree[]=new int[V];
+         for(int i=0;i<V;i++){
+            for(int v:adj.get(i)){
+                indegree[v]++;
             }
-        }
-        ArrayList<Integer> res=new ArrayList<>();
-        while(!st.isEmpty()){
-            res.add(st.pop());
-        }
-        return res;
+         }
+          Queue<Integer> q = new LinkedList<>();
+            for(int i=0;i<V;i++){
+                if(indegree[i]==0){
+                    q.add(i);
+                }
+            }
+            
+            //BFS
+            ArrayList<Integer> res=new ArrayList<>();
+            while(!q.isEmpty()){
+                int curr=q.poll();
+                res.add(curr);
+                for(int v:adj.get(curr)){
+                    indegree[v]--;
+                    if(indegree[v]==0){
+                        q.add(v);
+                    }
+                }
+            }
+            if(res.size()!=adj.size()){
+                return new ArrayList<>();
+            }
+            return res;
     }
-public static void DFS(ArrayList<ArrayList<Integer>> adj,int u,boolean []vis,Stack<Integer> st)
-        {
-             vis[u]=true;
-             for(int v:adj.get(u)){
-                 if(!vis[v]){
-                    DFS(adj,v,vis,st);
-                 }
-             }
-             st.push(u);
-        }
-    }
+}
